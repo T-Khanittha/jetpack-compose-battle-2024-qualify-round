8e06cf28fa8f7f7e560f80542c80ccf8c4c67566
T-Khanittha <tarn.khanit@odds.team>
2024-01-22 11:19:31 +0700

    [Create] Qualify1Screen

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     ksp("com.airbnb.android:showkase-processor:1.0.2")
     implementation("com.airbnb.android:showkase:1.0.2")
--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify1/Qualify1Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify1/Qualify1Screen.kt
@@ -1,13 +1,190 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify1
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 fun Qualify1Screen() {
-    // TODO: Qualify1
+    Column {
+        CenterAlignedTopAppBar()
+        ProfileCard()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CenterAlignedTopAppBar() {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = { },
+        navigationIcon = {
+            Image(painter = painterResource(id = R.drawable.ic_qualify_1_menu),
+                contentDescription = "",
+                modifier = Modifier.clickable { })
+
+        },
+        actions = {
+            Image(painter = painterResource(id = R.drawable.ic_qualify_1_profile),
+                contentDescription = "",
+                modifier = Modifier.clickable { })
+        },
+    )
+}
+
+@Composable
+fun ProfileCard() {
+    ConstraintLayout(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp, 16.dp, 16.dp, 72.dp)
+    ) {
+        val (profileCard, thumbContainer, profileImage, profileDetail) = createRefs()
+        ConstraintLayout(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .constrainAs(profileCard) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_qualify_1_profile),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .constrainAs(profileImage) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+            )
+            Column(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.primary.copy(alpha = 0.75f))
+                    .height(192.dp)
+                    .padding(20.dp)
+                    .constrainAs(profileDetail) {
+                        bottom.linkTo(profileImage.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                verticalArrangement = Arrangement.Top,
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    text = "John Doe",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    letterSpacing = MaterialTheme.typography.labelMedium.letterSpacing
+                )
+                Row(modifier = Modifier.height(20.dp)) {
+                    Icon(
+                        modifier = Modifier.padding(top = 2.dp, bottom = 2.dp),
+                        painter = painterResource(id = R.drawable.ic_qualify_1_gender_male),
+                        contentDescription = "gender male",
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 8.dp),
+                        text = "Male",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        letterSpacing = MaterialTheme.typography.labelMedium.letterSpacing
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(top = 8.dp, end = 19.dp)
+                        .height(60.dp),
+                    text = "Lorem ipsum dolor sit amet, cd nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    letterSpacing = MaterialTheme.typography.labelMedium.letterSpacing
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.constrainAs(thumbContainer) {
+                top.linkTo(profileCard.bottom)
+                bottom.linkTo(profileCard.bottom)
+                start.linkTo(profileCard.start)
+                end.linkTo(profileCard.end)
+            },
+            horizontalArrangement = Arrangement.spacedBy(43.dp)
+        ) {
+            ThumbDownButton {}
+            ThumbUpButton {}
+        }
+    }
+}
+
+@Composable
+fun ThumbDownButton(onClick: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .width(120.dp)
+            .height(48.dp),
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.errorContainer)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_qualify_1_thumb_down),
+            contentDescription = "thumb down"
+        )
+    }
+}
+
+@Composable
+fun ThumbUpButton(onClick: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .width(120.dp)
+            .height(48.dp),
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_qualify_1_thumb_up),
+            contentDescription = "thumb down"
+        )
+    }
 }
 
 // region Read-only because we use this to process your score.
